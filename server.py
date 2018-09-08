05b37114f04d48453cf0c5aece69ce997f84be01
Vishesh Kumar Nayak <visheshnayak@gmail.com>
2018-09-09 02:25:03 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+
+s.listen(1)
+
+while True:
+    conn, addr = s.accept()
+
+    print ('Connection addr: {}'.format(addr))
+
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+        print ('recieved data: {}'.format(data))
+        conn.send(data)
+
+    conn.close()
